aaaad40be20f4fc37588e7da312a7442f4605e96
nithya <nithukitty112@gmail.com>
2025-04-30 10:12:06 +0530

    changes

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app=express();
 // middleware to parse the request body
 app.use(express.json());
 app.use("/api/recipes",recipeRouter);
+app.get("/",(req,res)=>{
+    res.send("server is running..")
+})
 
 // middleware to handle 404 errors
 app.use(errorRoute);
